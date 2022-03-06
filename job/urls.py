0020402eafd2909720b06418364e9704787da776
unknown <anindha0004@gmail.com>
2022-03-06 17:22:14 +0600

    updated job 108

--- job/urls.py
+++ job/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('job',include('jobs.urls')),
-    # path('user',include('users.urls')),
+    path('user/',include('users.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-
+from django.contrib.auth.base_user import  AbstractBaseUser
 # Create your models here.
+
+
+
+class Account(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(_('email address'),unique=True)
+    first_name = models.CharField(_('first_name'),max_length=50,blank=False)
+    last_name = models.CharField(_('last_name'),max_length=50,blank=False)
+    date_joined = models.DateField(_())
